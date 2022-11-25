2a234e6ee1bce06f053f8dc003b103b04aa7459f
Ovijitnath <ovijitnathgn@gmail.com>
2022-11-26 02:14:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Find Uke server is running');
+})
+
+app.listen(port, () => console.log(`Find Uke running on ${port}`))
