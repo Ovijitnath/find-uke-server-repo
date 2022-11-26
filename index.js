cd1463d07e283e21d732dcf6aae32fec562a08c0
Ovijitnath <ovijitnathgn@gmail.com>
2022-11-27 02:03:08 +0600

    catagories added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -9,6 +11,29 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m7f1lqz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const catagoryCollection = client.db('findUke').collection('Catagories');
+
+        app.get('/Catagories', async (req, res) => {
+            const query = {}
+            const cursor = catagoryCollection.find(query);
+            const catagories = await cursor.toArray();
+            res.send(catagories);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.error(err));
 
 
 
