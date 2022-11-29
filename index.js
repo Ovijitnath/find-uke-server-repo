e1de34c4f7131eb752dfc0f631585d354fb055c5
Ovijitnath <ovijitnathgn@gmail.com>
2022-11-29 20:09:15 +0600

    my orders api created

--- index.js
+++ index.js
@@ -37,14 +37,30 @@ async function run() {
         });
 
 
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
-            // const query = {
-            //     appointmentDate: booking.appointmentDate,
-            //     email: booking.email,
-            //     treatment: booking.treatment 
-            // }
+            const query = {
+                booked: booking.productName,
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.booked}`
+                return res.send({ acknowledged: false, message })
+            }
+
+
             const result = await bookingsCollection.insertOne(booking)
             res.send(result);
         });
